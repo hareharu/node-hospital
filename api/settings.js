149e3ce811e4a88783964859241db6fd1be80e9d
Denis Larionov <mr.hareharu@gmail.com>
2021-03-02 14:42:36 +0700

    Редактирование параметров для модуля Оборудование

--- api/settings.js
+++ api/settings.js
@@ -31,4 +31,49 @@ router.get('/reset/:name', func.access('admin'), (req, res, next) => {
   });
 });
 
+router.get('/hardware/tags', func.access('admin'), (req, res, next) => {
+
+
+
+
+
+  conn.whodb.all("select *,\
+    case\
+    when class = 'computer' then 'Компьютер'\
+    when class = 'monitor' then 'Монитор'\
+    when class = 'printer' then 'Принтер'\
+    when class = 'scaner' then 'Сканер'\
+    when class = 'ups' then 'ИБП'\
+    when class = 'network' then 'Сетевое'\
+    end as rowgroup,\
+    id as key from hardwaretags order by class, pos, name", (error, data) => {
+    if (error) next(error);
+    res.data = data; next();
+  });
+});
+
+router.post('/hardware/tags/:action/:id', func.access('admin'), (req, res, next) => { 
+  switch (req.params.action) {
+    case 'delete':
+      req.values = [ req.params.id ];
+      req.query = "delete from hardwaretags where id = ?";
+      break;
+    case 'update':
+      req.values = [ req.body.name, req.body.type, req.body.pos, req.params.id];
+      req.query = "update hardwaretags set name = ?, type = ?, pos = ? where id = ?";
+      break;
+    case 'insert':
+      req.body.field = req.body.name.replace(/[^a-zа-я0-9]/gim,"").trim();
+      req.values = [ req.params.id, req.body.class, req.body.field, req.body.name, req.body.type, req.body.pos ];
+      req.query = "insert into hardwaretags (id, class, field, name, type, pos) values (?, ?, ?, ?, ?, ?)";
+      break;
+    default:
+      return res.sendStatus(404);
+  }
+  conn.whodb.run(req.query, req.values, function(err) {
+    if (err) return next(err);
+    res.sendStatus(200);
+  });
+});
+
 module.exports = router;
--- src/modules/system/Settings.tsx
+++ src/modules/system/Settings.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Module, { Table, callAPI, callAPIPost, openDialog, getItems } from 'components';
+import Module, { Table, TabsLinks, TabsContainer, callAPI, callAPIPost, openDialog, openEditPanel, getItems, uuid } from 'components';
 
 interface ISetting {
   name: string,
@@ -8,30 +8,85 @@ interface ISetting {
   description: string,
 }
 
+interface IHardwareTag {
+  id: string,
+  key: string,
+  class: string,
+  field: string,
+  name: string,
+  type: string,
+  pos: number,
+}
+
 export default function ModuleSettings({...props}) {
-  const [items, setItems] = useState<ISetting[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [selected, setSelected] = useState<ISetting | undefined>(undefined);
-  const [reload, forceReload] = useState(false);
- 
-  const onSelect = (item: ISetting | undefined) => setSelected(item);
-  const onReset = () => openDialog('Сброс параметра', 'Параметр будет сброшен к значению по умолчанию.', () => callAPI('api/settings/reset/'+selected?.name, undefined, () => forceReload(!reload)));
-  const onEdit = () => openDialog('Изменение параметра', 'Укажите новое значение', (value) => callAPIPost('api/settings/value/'+selected?.name, { value }, undefined, ()=> forceReload(!reload)), selected?.type, selected?.value);
-  const commands = [
-    { disabled: !selected, key: 'edit', name: 'Изменить', iconProps: { iconName: 'icon-edit-2' }, onClick: onEdit },
-    { disabled: !selected, key: 'reset', name: 'Сбросить', iconProps: { iconName: 'icon-trash' }, onClick: onReset },
-    { key: 'refresh', name: 'Обновить', iconProps: { iconName: 'icon-refresh-cw' }, onClick: () => forceReload(!reload) },
+  const [itemsSetting, setItemsSetting] = useState<ISetting[]>([]);
+  const [loadingSetting, setLoadingSetting] = useState(false);
+  const [selectedSetting, setSelectedSetting] = useState<ISetting | undefined>(undefined);
+  const [reloadSetting, forceReloadSetting] = useState(false);
+  const [itemsHardwareTag, setItemsHardwareTag] = useState<IHardwareTag[]>([]);
+  const [loadingHardwareTag, setLoadingHardwareTag] = useState(false);
+  const [selectedHardwareTag, setSelectedHardwareTag] = useState<IHardwareTag | undefined>(undefined);
+  const [reloadHardwareTag, forceReloadHardwareTag] = useState(false);
+  const [tabIndex, setTabIndex] = useState(0);
+  const onSelectSetting = (item: ISetting | undefined) => setSelectedSetting(item);
+  const onResetSetting = () => openDialog('Сброс параметра', 'Параметр будет сброшен к значению по умолчанию.', () => callAPI('api/settings/reset/'+selectedSetting?.name, undefined, () => forceReloadSetting(!reloadSetting)));
+  const onEditSetting = () => openDialog('Изменение параметра', 'Укажите новое значение', (value) => callAPIPost('api/settings/value/'+selectedSetting?.name, { value }, undefined, ()=> forceReloadSetting(!reloadSetting)), selectedSetting?.type, selectedSetting?.value);
+  const onSelectHardwareTag = (item: IHardwareTag | undefined) => setSelectedHardwareTag(item);
+  const onDeleteHardwareTag = () => openDialog('Удаление параметра', 'Параметр "'+selectedHardwareTag?.name+'" будет удален.', () => callAPIPost('api/settings/hardware/tags/delete/'+selectedHardwareTag?.id, undefined, undefined, () => forceReloadHardwareTag(!reloadHardwareTag)));
+  const onEditHardwareTag = () => openEditPanel(
+    (selectedHardwareTag ? 'Изменить': 'Добавить' )+' параметр', [
+      { disabled: selectedHardwareTag !== undefined, key: 'class', type: 'select', value: selectedHardwareTag?.class, label: 'Класс', options: [
+        { key: 'computer', text: 'Компьютер'},
+        { key: 'monitor', text: 'Монитор'},
+        { key: 'printer', text: 'Принтер'},
+        { key: 'scaner', text: 'Сканер'},
+        { key: 'ups', text: 'ИБП'},
+        { key: 'network', text: 'Сетевое'}
+      ]},
+      // { disabled: selectedHardwareTag !== undefined, key: 'field', type: 'text', value: selectedHardwareTag?.field, label: 'Код' },
+      { key: 'name', type: 'text', value: selectedHardwareTag?.name, label: 'Имя' },
+      { key: 'type', type: 'select', value: selectedHardwareTag?.type, label: 'Тип', options: [
+        {key:'input', text: 'Текст'},
+        {key:'date', text: 'Дата'},
+        {key:'select', text: 'Список'},
+        {key:'toggle', text: 'Да/Нет'}
+      ]},
+      { key: 'pos', type: 'number', value: selectedHardwareTag?.pos.toString() || '0', label: 'Позиция' }
+    ], (values: IHardwareTag) => callAPIPost('api/settings/hardware/tags/'+(selectedHardwareTag?'update':'insert')+'/'+(selectedHardwareTag?.id || uuid()), values, undefined, () => forceReloadHardwareTag(!reloadHardwareTag))
+    , ['class', 'name', 'type']);
+  const onEditHardwareSelect = () => {}
+  const commandsSetting = [
+    { disabled: !selectedSetting, key: 'edit', name: 'Изменить', iconProps: { iconName: 'icon-edit-2' }, onClick: onEditSetting },
+    { disabled: !selectedSetting, key: 'reset', name: 'Сбросить', iconProps: { iconName: 'icon-trash' }, onClick: onResetSetting },
+    { key: 'refresh', name: 'Обновить', iconProps: { iconName: 'icon-refresh-cw' }, onClick: () => forceReloadSetting(!reloadSetting) },
   ];
-  const columns = [
+  const columnsSetting = [
     { key: 'df541f5f-107f-4660-baef-e49032b79aa1', name: 'Код', fieldName: 'name', minWidth: 100, maxWidth: 150, isResizable: true },
     { key: '29856703-cfe6-40bd-9f5a-4256f9f94653', name: 'Тип', fieldName: 'type', minWidth: 70, maxWidth: 70, isResizable: true },
     { key: '0f1f7894-58fe-40f8-a283-c33e698ec41b', name: 'Значение', fieldName: 'value', minWidth: 100, maxWidth: 200, isResizable: true },
     { key: '32414418-d2c7-4f00-8d19-e80d65ee1131', name: 'Описание', fieldName: 'description', minWidth: 150, maxWidth: 200, isResizable: true},
   ];
-  useEffect(() => getItems('api/settings/list', setItems, setLoading), [reload]);
+  const commandsHardwareTag = [
+    { disabled: selectedHardwareTag !== undefined, key: 'add', name: 'Добавить', iconProps: { iconName: 'icon-plus' }, onClick: onEditHardwareTag },
+    { disabled: !selectedHardwareTag, key: 'edit', name: 'Изменить', iconProps: { iconName: 'icon-edit-2' }, onClick: onEditHardwareTag },
+    { disabled: !selectedHardwareTag, key: 'reset', name: 'Удалить', iconProps: { iconName: 'icon-trash' }, onClick: onDeleteHardwareTag },
+    { disabled: !selectedHardwareTag || selectedHardwareTag.type != 'select', key: 'select', name: 'Значения', iconProps: { iconName: 'icon-clipboard' }, onClick: onEditHardwareSelect },
+    { key: 'refresh', name: 'Обновить', iconProps: { iconName: 'icon-refresh-cw' }, onClick: () => forceReloadHardwareTag(!reloadHardwareTag) },
+  ];
+  const columnsHardwareTag = [
+    { key: '1ab452f5-2346-4d88-afb3-d3fe7b8c0b51', name: 'Имя', fieldName: 'name', minWidth: 200, maxWidth: 300, isResizable: true },
+    { key: 'd2d71d05-905b-4c52-884a-fb0207653f6b', name: 'Тип', fieldName: 'type', minWidth: 100, maxWidth: 150, isResizable: true},
+    { key: '5767209f-5427-4c6e-bb6c-9e36517c4bd2', name: 'Позиция', fieldName: 'pos', minWidth: 50, maxWidth: 100, isResizable: true},
+  ];
+  useEffect(() => getItems('api/settings/list', setItemsSetting, setLoadingSetting), [reloadSetting]);
+  useEffect(() => getItems('api/settings/hardware/tags', setItemsHardwareTag, setLoadingHardwareTag), [reloadHardwareTag]);
   return (
     <Module {...props}>
-      <Table commands={commands} onSelect={onSelect} items={items} columns={columns} loading={loading}/>
+      <TabsLinks links={['Система', 'Оборудование']} onClick={(value) => setTabIndex(value)} tabIndex={tabIndex}/>
+      <TabsContainer tabIndex={tabIndex}>
+        <Table commands={commandsSetting} onSelect={onSelectSetting} items={itemsSetting} columns={columnsSetting} loading={loadingSetting}/>
+        <Table commands={commandsHardwareTag} onSelect={onSelectHardwareTag} items={itemsHardwareTag} columns={columnsHardwareTag} loading={loadingHardwareTag} grouped={true}/>
+      </TabsContainer>
     </Module>
   );
 }
